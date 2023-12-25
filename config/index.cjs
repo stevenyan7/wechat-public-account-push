cf7a8e4400e88a60ad08ac6be501f9a785ee676e
stevenyan7 <stevenyan2015@gmail.com>
2023-12-25 21:58:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -6,26 +6,26 @@
  * 自行添加属性，以支持更多个性化功能
  */
 const USER_CONFIG = {
-
+  USE_PASSAGE: 'push-deer',
   // 使用微信测试号：公众号APP_ID
   APP_ID: '',
 
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河南',
+  CITY: '郑州',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '宝宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oWQCV6pkD7GeRunanfwhHluhotOg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '07-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
